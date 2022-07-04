b2c0d68dff6d5d1193e34f1a28eacacbcb1d2db7
yeppyshiba <100031060+yeppyshiba@users.noreply.github.com>
2022-07-04 10:44:06 +0900

    Update Loader.ts

--- src/scenes/Loader.ts
+++ src/scenes/Loader.ts
@@ -4,11 +4,25 @@ import { Container } from "pixi.js";
 import AbstractGameScene from "@/abstracts/scenes/AbstractGameScene";
 
 class Loader extends AbstractGameScene {
-    setup(sceneContainer: Container): void {}
+    setup(sceneContainer: Container): void {
+        this.sceneState = SceneState.LOAD;
+        
+        const loader = this.getLoader();
+        loader.onComplete.once(this.onLoadComplete);
+        loader.onError.once(this.onLoadError);
+    }
 
     preTransitionUpdate(delta: number): void {}
 
     sceneUpdate(delta: number): void {}
+    
+    private onLoadComplete(): void {
+        console.log("onLoadComplete");
+    }
+
+    private onLoadError(): void {
+        console.log("onLoadError");
+    }    
 }
 
 export default Loader;
