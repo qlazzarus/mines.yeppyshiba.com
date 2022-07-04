8b72b3884ce50e5a41442ebcde8e2c417ac3bee4
monoless <ecst@monolife.co.kr>
2022-07-04 20:05:29 +0900

    adding loader event

--- src/scenes/Loader.ts
+++ src/scenes/Loader.ts
@@ -2,27 +2,28 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Container } from "pixi.js";
 import AbstractGameScene from "@/abstracts/scenes/AbstractGameScene";
+import SceneState from "@/enums/SceneState";
 
 class Loader extends AbstractGameScene {
     setup(sceneContainer: Container): void {
         this.sceneState = SceneState.LOAD;
-        
+
         const loader = this.getLoader();
-        loader.onComplete.once(this.onLoadComplete);
-        loader.onError.once(this.onLoadError);
+        loader.onComplete.once(this.onLoadComplete.bind(this));
+        loader.onError.once(this.onLoadError.bind(this));
     }
 
     preTransitionUpdate(delta: number): void {}
 
     sceneUpdate(delta: number): void {}
-    
+
     private onLoadComplete(): void {
-        console.log("onLoadComplete");
+        this.sceneSwitcher("clockwise");
     }
 
     private onLoadError(): void {
         console.log("onLoadError");
-    }    
+    }
 }
 
 export default Loader;
